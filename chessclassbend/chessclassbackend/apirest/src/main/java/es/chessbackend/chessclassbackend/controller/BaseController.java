17ce778d28e69b0e933eaf672be8ccb4fd1085af
Marcos Martínez Fernández <118571834+IsFriskis@users.noreply.github.com>
2025-10-13 18:03:18 +0200

    Refactor module to domain and add BaseController
    Renamed the 'module' Maven module to 'domain', updating all relevant paths and references. Added a new BaseController in the apirest module with a status endpoint. Updated dependencies in apirest and boot modules, including MapStruct, Apache POI, and Spring Boot versions. Adjusted project configuration files to reflect these changes.

---
+++ chessclassbend/chessclassbackend/apirest/src/main/java/es/chessbackend/chessclassbackend/controller/BaseController.java
@@ -0,0 +1,28 @@
+package es.chessbackend.chessclassbackend.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/chessapp")
+public class BaseController {
+
+    @GetMapping(value = "/status")
+    public ResponseEntity<String> status() {
+        return new ResponseEntity<>("BaseController Up", getHeader(), HttpStatus.OK);
+    }
+
+    private HttpHeaders getHeader() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/json");
+        headers.add("Access-Control-Allow-Origin"," http://localhost:3000");
+        return headers;
+    }
+}
